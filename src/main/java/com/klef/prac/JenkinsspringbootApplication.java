288f162fa4ae84cfbabe6c525b2bc4fbe38beb29
SOMESH <someshbiswal2308@gmail.com>
2025-10-15 01:51:26 +0530

    Changed pom.xml and Main Class

--- src/main/java/com/klef/prac/JenkinsspringbootApplication.java
+++ src/main/java/com/klef/prac/JenkinsspringbootApplication.java
@@ -2,17 +2,10 @@ package com.klef.prac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class JenkinsspringbootApplication extends SpringBootServletInitializer {
-
-    // Required for WAR deployment
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(JenkinsspringbootApplication.class);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsspringbootApplication.class, args);
