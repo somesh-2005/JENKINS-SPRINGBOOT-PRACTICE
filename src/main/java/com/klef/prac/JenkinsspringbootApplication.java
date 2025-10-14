05adf23722d7333d7c1e15e4c334d4d14b46e85a
SOMESH <someshbiswal2308@gmail.com>
2025-10-15 01:34:35 +0530

    Changed pom.xml and Main Class

--- src/main/java/com/klef/prac/JenkinsspringbootApplication.java
+++ src/main/java/com/klef/prac/JenkinsspringbootApplication.java
@@ -2,14 +2,19 @@ package com.klef.prac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsspringbootApplication extends SpringBootServletInitializer{
+public class JenkinsspringbootApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenkinsspringbootApplication.class, args);
-		System.out.println("My Project is working");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JenkinsspringbootApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsspringbootApplication.class, args);
+        System.out.println("My Project is working");
+    }
 }
